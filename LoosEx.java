6549468e016cb7824188242e30f9ac7ced8799cb
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-06-17 15:39:50 +0530

    loops practice

---
+++ LoosEx.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class LoosEx {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        //  System.out.println("enter n : ");
+        //   int n = sc.nextInt();
+
+        // System.out.println("enter n ");
+        // int n =sc.nextInt();
+        // for(int i=1; i<=n; i++){
+        //     System.out.println(i);
+        // }
+
+    //   while(n>1){
+    //     System.out.println(n);  //reverse using while
+    //     n--;
+    //   }
+
+
+
+         //  System.out.println("enter n : ");
+          //  int n =sc.nextInt();
+         // for(int i=n; i>=1; i--){
+         //     System.out.println(i);
+ 
+
+    //    for(int i=1; i<=10 ; i++){
+        
+    //     System.out.println("number:"+ n * i);
+    //    }
+
+   
+    //   String name= "Shrutika";
+
+    //    for(int i=1;i<=100; i++){
+    //       System.out.println(name);
+    //   }
+
+
+//     for(int num=2 ; num <=100; num++)
+//    {
+//      boolean isPrime=true;
+//      for(int i=2 ; i<num; i++){
+//         if(num% i==0){
+//             isPrime=false;
+//             break;      
+//         }
+//      }
+
+//      if(isPrime){
+//         System.out.println(num+ " ");
+//      }
+//    }
+
+
+//even
+
+// for(int i=1; i<=100; i++){
+//     if(i%2==0){
+//         System.out.println(i);
+//     }
+// }
+
+
+// System.out.println("entern number :");
+// int n =sc.nextInt();
+
+// int sum=0;
+
+// for(int i=1; i<=n; i++){
+// sum=sum+i;
+
+// System.out.println(sum);
+//}
+
+       
+
+
+        for(int i = 500; i>=100; i--){
+            if(i%7==0){
+                System.out.println(i);
+            }
+        }
+
+
+ sc.close();
+    }
+}
