a9a6a580810972647b384dc7bb13bd50efc84476
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-07-14 19:33:19 +0530

    basics rivision

---
+++ BasicsRevision.java
@@ -0,0 +1,11 @@
+public class BasicsRevision{
+    int age ;
+
+
+    public static void main(String[] args) {
+        int age =0;
+        System.out.println(age);
+    // System.out.println(age);
+
+    }
+}
