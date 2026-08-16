566aa46027eb11e9a3de176eed19861092180f89
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-08-16 22:22:33 +0530

    inheritance practice

---
+++ Oops/3_Inheritance/Country.java
@@ -0,0 +1,21 @@
+class state{
+
+state(){
+    System.out.println("states in India...");
+}
+}
+class Maharashtra extends state{
+   Maharashtra(){
+System.out.println("maharashtra state in india");
+   }
+}
+
+public class Country{
+public static void main(String[] args) {
+ 
+    Maharashtra mh= new Maharashtra();
+    
+
+}
+
+}
