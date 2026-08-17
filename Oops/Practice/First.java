4a847353178c57be7b61c510f0fa36d2e0f6d97f
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-08-17 18:39:06 +0530

    class and object

---
+++ Oops/Practice/First.java
@@ -0,0 +1,23 @@
+public class First {
+String color;
+String brand;
+
+   void start(String brand){
+    brand= "toyota";
+    System.out.println("car is starting..");
+    System.out.println("brand is " + brand );
+   } 
+
+   void stop(String color){
+    color="yellloww";
+    System.out.println("car is stopping..");
+    System.out.println("color: " + color);
+   }
+    public static void main(String[] args) {
+        
+        First f= new First();
+        f.start("toyota");
+        f.stop("pink");
+
+    }
+}
