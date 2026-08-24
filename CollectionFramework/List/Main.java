9cc349389036c5ee5cb87973617c2f02f0728ba1
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-08-24 22:49:16 +0530

    collection framework

---
+++ CollectionFramework/List/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+
+      
+       names.add("mummy");
+       names.add("pappa");
+       names.add("bhaiyya");
+       names.add("mai");
+       System.out.println(names);
+    }
+
+}
