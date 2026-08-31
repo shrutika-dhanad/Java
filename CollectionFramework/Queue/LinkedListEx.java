ff30f5916b329e5874bfad4721f700a6ffe40cca
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-08-31 17:10:50 +0530

    queue interface with its implementations

--- CollectionFramework/Queue/LinkedListEx.java
+++ CollectionFramework/Queue/LinkedListEx.java
@@ -5,8 +5,8 @@ public class LinkedListEx {
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
 
-        //normal queue mdhe insertion always rear se hoil and removela from the front
-        
+        // normal queue mdhe insertion always rear se hoil and removela from the front
+
         // q.add(10);
         // System.out.println(q);
         // q.offer(120);
@@ -16,24 +16,25 @@ public class LinkedListEx {
 
         // System.out.println(q.element()); //throw exception when no element is presetn
 
-        System.out.println(q.peek()); //return null if no ele is presetn.
+        System.out.println(q.peek()); // return null if no ele is presetn.
         // System.out.println();
 
-
         q.offer(10);
+        q.offer(110);
+
         q.offer(20);
         q.offer(30);
         q.offer(40);
-        q.offer(50);  
+        q.offer(50);
 
-         System.out.println(q);
+        System.out.println(q);
 
-         q.remove();  //throw exception when no ele is present.
+        q.remove(); // throw exception when no ele is present.
 
-         System.out.println(q);
+        System.out.println(q);
 
-System.out.println(q.poll());
-System.out.println(q);
+        System.out.println(q.poll());
+        System.out.println(q);
 
     }
 }
