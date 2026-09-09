100c29ada23ac5ba1d638e3c0c727d270b14e554
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-09-09 18:45:18 +0530

    Average of all array elements

---
+++ DSA/01_ArrayProblems/One_Avg_of_Arr_Elements.java
@@ -0,0 +1,22 @@
+
+//O(n) - time complexity and O(1) - is space complexity.
+
+public class One_Avg_of_Arr_Elements {
+    static  double getAvg(int[] arr){
+    double sum=0;
+    for(int i : arr){
+        sum += i;
+
+    }
+    int size = arr.length;
+    double avg =sum /size;
+    return  avg;
+ }
+    public static void main(String[] args) {
+        int arr[]={1,9,6,3,8,1,4};
+      System.out.println(getAvg(arr));
+
+
+    }
+    
+}
