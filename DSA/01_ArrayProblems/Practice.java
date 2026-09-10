41e7e62fd3da24b3e9cfc7ae20fb993b5d7419af
Shrutika_Dhanad <dhanadshrutika@gmail.com>
2026-09-10 17:20:12 +0530

    practice of examples

--- DSA/01_ArrayProblems/Practice.java
+++ DSA/01_ArrayProblems/Practice.java
@@ -21,48 +21,45 @@
 // }
 
 
-class Practice{
-    static  double getAverage(int[] arr){
-        double sum =0;
-        for(int  i : arr){
-            sum+= i;
-        }
-        int size = arr.length;
-        double average= sum/size;
-        return average;
-
-    }
-    public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50};
-        System.out.println(getAverage(arr));
-    }
-}
-
-
-
 // class Practice{
-
-//     static int multByTten(int[] arr){
-    
-//        int size= arr.length;
-//     int[] newArray = new int[size];
-//     for(int i=0; i<size; i++){
-//         int element = arr[i];
-//         int newElement = element*10;
-//           newArray[i]= newElement;
-
-//     }
-
-
-        
+//     static  double getAverage(int[] arr){
+//         double sum =0;
+//         for(int  i : arr){
+//             sum+= i;
+//         }
+//         int size = arr.length;
+//         double average= sum/size;
+//         return average;
 
 //     }
 //     public static void main(String[] args) {
-//         int arr[] = {1,2,3,4,5,6,8,9,6};
-//         int ans[] = multByTten(arr);
-//             for(int i : ans ){
-//             System.out.println(i);
-
-//         }
+//         int[] arr = {10,20,30,40,50};
+//         System.out.println(getAverage(arr));
 //     }
 // }
+
+
+
+
+
+public class Practice {
+
+    static  int[] multiplyByTen(int[] arr){
+        int size=  arr.length;
+        int[] newArr= new int[size];
+     for(int i=0; i<size; i++){
+        int element= arr[i];
+        int newElement = element*10;
+         newArr[i] = newElement;
+     }
+     return  newArr;
+
+    }
+    public static void main(String[] args) {
+        int[] arr= {10,20,30,40};
+        int answer[] = multiplyByTen(arr);
+        for(int i : answer){
+            System.out.println("answer : " +i);
+        }
+    }
+}
